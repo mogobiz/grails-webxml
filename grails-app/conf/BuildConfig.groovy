68cbcf8c90a98c03f9954c5770388b9ef10b5f8a
Burt Beckwith <beckwithb@vmware.com>
2012-05-30 20:20:00 -0400

    update to Grails 2.0.4 and release 2.0.2

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,5 +1,6 @@
 grails.project.work.dir = 'target'
 grails.project.docs.output.dir = 'docs/manual'
+grails.project.source.level = 1.6
 
 grails.project.dependency.resolution = {
 
@@ -8,5 +9,11 @@ grails.project.dependency.resolution = {
 
 	repositories {
 		grailsCentral()
+	}
+
+	plugins {
+		build(':release:2.0.2', ':rest-client-builder:1.0.2') {
+			export = false
+		}
 	}
 }
